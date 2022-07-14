a35a7ba4420974c48c6c65d754d405a0a19f8720
893077685 <87535251+893077685@users.noreply.github.com>
2022-07-14 21:33:18 +0800

    Upload by PicGo

---
+++ abc/服务通信_服务方.cpp
@@ -0,0 +1,43 @@
+/*
+
+    服务器实现:
+        1.包含头文件
+        2.初始化 ROS 节点
+        3.创建 ROS 句柄
+        4.创建 服务 对象
+        5.回调函数处理请求并产生响应
+        6.由于请求有多个，需要调用 ros::spin()
+
+*/
+#include "ros/ros.h"
+#include "plumbing_server_client/AddInts.h"
+
+// bool 返回值由于标志是否处理成功
+bool doReq(plumbing_server_client::AddInts::Request& req,
+           plumbing_server_client::AddInts::Response& resp){
+    int num1 = req.num1;
+    int num2 = req.num2;
+
+    ROS_INFO("服务器接收到的数据为:num1 = %d, num2 = %d",num1, num2);
+
+
+    //如果没有异常，那么相加并将结果赋值给 resp
+    resp.sum = num1 + num2;
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    setlocale(LC_ALL,"");
+    // 2.初始化 ROS 节点
+    ros::init(argc,argv,"AddInts_Server");
+    // 3.创建 ROS 句柄
+    ros::NodeHandle nh;
+    // 4.创建 服务 对象
+    ros::ServiceServer server = nh.advertiseService("AddInts",doReq);
+    ROS_INFO("服务启动!");
+    //     5.回调函数处理请求并产生响应
+    //     6.由于请求有多个，需要调用 ros::spin()
+    ros::spin();
+    return 0;
+}
